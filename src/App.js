d50f09478ef230b8504923d5b3793b696fdc2df1
surangasandaruwan <mpsurangasw1111@gmail.com>
2024-10-26 17:49:06 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./mylogo.png";
 import "./App.css";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload...</p>
+        <p>Its me Suranga</p>
         <a
           className="App-link"
           href="https://reactjs.org"
